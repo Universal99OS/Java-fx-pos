31f86688344d4733b634b35b0c10caba3db67baa
Universal99OS <pandukapiushanpullaperuma4321@gmail.com>
2023-12-15 16:44:12 +0530

    create-orders-view

---
+++ src/main/java/Controller/OrderFormController.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.scene.control.TreeTableColumn;
+
+public class OrderFormController {
+    public JFXTreeTableView orderTableId;
+    public TreeTableColumn colOrderId;
+    public TreeTableColumn colDate;
+    public TreeTableColumn colCusId;
+    public TreeTableColumn colNameId;
+    public TreeTableColumn colAmountId;
+    public TreeTableColumn colOptionId;
+    public JFXTreeTableView orderDetailsTableId;
+    public TreeTableColumn colItemId;
+    public TreeTableColumn colDesId;
+    public TreeTableColumn colUnitPriceId;
+    public TreeTableColumn colQtyId;
+    public TreeTableColumn colAmountItemId;
+    public TreeTableColumn colOptionItemId;
+
+
+}
