cbd54309152873aacf585c1f1de5528e5a7b45e3
Universal99OS <pandukapiushanpullaperuma4321@gmail.com>
2023-12-22 16:11:11 +0530

    initializ the hybernate util

--- src/main/java/Entity/Item.java
+++ src/main/java/Entity/Item.java
@@ -2,12 +2,17 @@ package Entity;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
+@Entity
 public class Item {
+    @Id
     private String code;
     private String description;
     private double unitPrice;
